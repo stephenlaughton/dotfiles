a24afea4d69497bc2fccfcd7e2ed7e196abf35e3
Stephen Laughton <stephenlaughton@mixdog.io>
2015-05-01 18:35:28 -0700

    Fix up install.sh to allow for arbitrary location of dotfiles.

--- install/link.sh
+++ install/link.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo "----------- we are in $DIR"
 
-DOTFILES=$HOME/.dotfiles
+OLDDOTFILES=$HOME/.old_dotfiles
 
-echo "creating symlinks"
+# create dotfiles_old in homedir
+echo "creating $OLDDOTFILES for backup of any existing dotfiles in $HOME"
+mkdir -p $OLDDOTFILES
+echo "...done"
+
+# move any existing dotfiles into .old_dotfiles and then link
+echo "moving any old dotfiles into .old_dotfiles and creating symlinks"
 linkables=$( ls -1 -d **/*.symlink )
 for file in $linkables ; do
     target="$HOME/.$( basename $file ".symlink" )"
+    echo "Moving any existing dotfiles from ~ to $OLDDOTFILES"
+    mv $target $OLDDOTFILES
     echo "creating symlink for $file"
-    ln -s $DOTFILES/$file $target
+    ln -s $DIR/$file $target
 done
