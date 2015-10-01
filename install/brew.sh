85feade2740168c8430ace6c15cea0ccb479a87e
Stephen Laughton <slaughton@xydrone.com>
2015-10-01 11:58:44 -0700

    cleanup brew file

--- install/brew.sh
+++ install/brew.sh
@@ -29,25 +29,39 @@ brew install caskroom/cask/brew-cask
 
 # apps
 brew cask install 1password
+brew cask install appcleaner
+brew cask install atom
+apm install atom-beautify
+apm install emmet
+apm install file-icons
+apm install pigments
+apm install vim-mode
+brew cask install brackets
+brew cask install codekit
+brew cask install daisydisk
+brew cask install dash
+brew cask install dockertoolbox
 brew cask install dropbox
+brew cask install duet
+brew cask install firefox
+brew cask install flux
 brew cask install github
+brew cask install gitter
 brew cask install google-chrome
 brew cask install google-chrome-canary
+brew cask install handbrake 
 brew cask install istat-menus
 brew cask install iterm2
+brew cask install karabiner
+brew cask install mailbox
+brew cask install opera
+brew cask install screenhero
+brew cask install selfcontrol
+brew cask install skype
 brew cask install spectacle
 brew cask install sublime-text3
+brew cask install things
 brew cask install transmission
-brew cask install flux
-brew cask install appcleaner
-brea cask install atom
-brew cask install screenhero
-brew cask install firefox
-brew cask install opera
-brew cask install skype
 brew cask install visual-studio-code
-brew cask install selfcontrol
-brew cask install brackets
-brew cask install dockertoolbox
-brew cask install dash
+brew cask install vlc
 
