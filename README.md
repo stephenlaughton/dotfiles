912492e55281b52288fe9efced854c34e1e441e7
Stephen Laughton <stephenlaughton@gmail.com>
2019-07-18 13:33:09 -0700

    update stuff and readme

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # dotfiles
-My dotfiles
+Dotfiles for easily setting up a new Jenkins machine with:
+- homebrew
+  - git
+  - ruby
+  - jenkins
+  - openjdk flavour java 8
+  - fish shell with config
+  - nodenv with node `8.11.4`
+  - yarn
+  - carthage
+  - vscode insiders
+  - chrome
+  - firefox
+  - android studio
+  - istat menu
+  - realm studio
+  - slack
+  - alfred
 
-## todo
-- fish config
-- fix zsh
-- work on hammerspoon window stuff
+# New Install
+
+1. In terminal `mkdir -p Dev/github.com/invoice-simple && cd Dev/github.com/invoice-simple && git clone https://github.com/invoice-simple/dotfiles.git && cd dotfiles && ./install.sh`
+2. Wait for a bit and make sure everything installs without errors
+3. Profit
--- install/brew.sh
+++ install/brew.sh
@@ -1,72 +1,37 @@
 #!/bin/sh
 # give me what I need, brew!
 
-# development tools
-brew install git
-brew install vim
-brew install zsh
-# zplugin
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
-
 # get cask versions also
 brew tap caskroom/versions
+
+# development tools
+brew install git
+# Node stuff
+brew install nodenv
+# grab stable version of node
+nodenv install 8.11.4
+# convenience plugin for nodenv
+git clone https://github.com/nodenv/nodenv-package-rehash.git "$(nodenv root)"/pluginsnodenv-package-rehash
+nodenv package-hooks install --all
+brew install yarn --ignore-dependencies
+brew install ruby
+brew install m-cli
+brew install grep
+brew install watchman
+brew install exa
+brew install fish
+brew install carthage
+brew cask install adoptopenjdk8
 
 # apps
 brew cask install alfred
 brew cask install appcleaner
-brew cask install carbon-copy-cloner
-brew cask install daisydisk
-brew cask install dropbox
 brew cask install google-chrome
 brew cask install hammerspoon
 brew cask install istat-menus
 brew cask install iterm2
-brew cask install skype
-brew cask install transmission
-brew cask install vlc
-brew cask install 1password
 brew cask install kap
-
-echo ""
-echo "Install Extra Dev Stuff? (y/n)"
-read -r response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$  ]]; then
-    # cli tools
-    brew install ack
-    brew install tree
-    brew install wget
-
-    # development server setup
-    brew install nginx
-
-    # development tools
-    brew install hub
-    brew install reattach-to-user-namespace
-    brew install highlight
-    brew install svn
-
-    # Node stuff
-    brew install nodenv
-    # grab stable version of node
-    nodenv install 8.11.4
-    # convenience plugin for nodenv
-    git clone https://github.com/nodenv/nodenv-package-rehash.git "$(nodenv root)"/plugins/nodenv-package-rehash
-    nodenv package-hooks install --all
-    brew install yarn --ignore-dependencies
-
-    brew install ruby
-    brew install m-cli
-    brew install git-flow
-    brew install grep
-    brew install watchman
-    brew install exa
-    brew install fish
-
-    # dev casks
-    brew cask install dash
-    brew cask install duet
-    brew cask install firefox
-    brew cask install visual-studio-code
-    brew cask install slack
-    brew cask install docker
-fi
+brew cask install firefox
+brew cask install visual-studio-code-insiders
+brew cask install slack
+brew cask install realm-studio
