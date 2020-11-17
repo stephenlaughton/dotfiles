e5a1812b3c8d82b1c1d4f01e8c3c6a21b174b0a3
Stephen Laughton <stephenlaughton@gmail.com>
2020-11-17 12:29:58 -0800

    add and remove some casks

--- install/brew.sh
+++ install/brew.sh
@@ -11,11 +11,10 @@ brew install svn
 # Node stuff
 brew install nodenv
 # grab stable version of node
-nodenv install 10.16.3
+nodenv install 12.16.1
 # convenience plugin for nodenv
 git clone https://github.com/nodenv/nodenv-package-rehash.git "$(nodenv root)"/pluginsnodenv-package-rehash
 nodenv package-hooks install --all
-brew install yarn --ignore-dependencies
 brew install ruby
 brew install m-cli
 brew install grep
@@ -36,15 +35,15 @@ brew cask install appcleaner
 brew cask install google-chrome
 brew cask install hammerspoon
 brew cask install istat-menus
-brew cask install iterm2
 brew cask install kap
 brew cask install firefox
 brew cask install visual-studio-code-insiders
 brew cask install slack
-brew cask install realm-studio
 brew cask install the-unarchiver
 brew cask install 1password
 brew cask install telegram
+brew cask install figma
+brew cask install notion
 
 # fonts
 brew cask install font-jetbrains-mono
