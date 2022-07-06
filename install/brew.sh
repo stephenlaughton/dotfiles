5853e7c7e8aa13394f9c44b357148a61dcefa8a2
Stephen Laughton <stephenlaughton@gmail.com>
2022-07-05 18:18:55 -0700

    change a couple versions

--- install/brew.sh
+++ install/brew.sh
@@ -11,13 +11,13 @@ brew install svn
 # Node stuff
 brew install nodenv
 # grab stable version of node
-nodenv install 14.17.0
+nodenv install 14.19.3
 # convenience plugin for nodenv
 git clone https://github.com/nodenv/nodenv-package-rehash.git "$(nodenv root)"/pluginsnodenv-package-rehash
 git clone https://github.com/nodenv/nodenv-aliases.git $(nodenv root)/plugins/nodenv-aliases
 nodenv alias --auto
 nodenv package-hooks install --all
-brew install temurin # java runtime
+brew install temurin11 # java runtime
 brew install bash
 brew install carthage
 brew install exa
@@ -47,6 +47,7 @@ brew install istat-menus
 brew install iterm2
 brew install kap
 brew install linear-linear
+brew install insomnia
 brew install notion
 brew install responsively
 brew install slack
