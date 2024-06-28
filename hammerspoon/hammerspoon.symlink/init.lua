9e2605718f609fa10307875f0a3d9eb4f39a013c
Stephen Laughton <stephenlaughton@gmail.com>
2024-06-28 10:24:37 -0700

    get rid of hammerspoon window management since raycast offers it

--- hammerspoon/hammerspoon.symlink/init.lua
+++ hammerspoon/hammerspoon.symlink/init.lua
@@ -30,108 +30,108 @@ end, nil, function ()
   myDoKeyStroke({""}, "right")
 end)
 
-hs.hotkey.bind({"cmd", "alt"}, "f", function()
-  local win = hs.window.focusedWindow()
-  local screen = win:screen()
-  local max = screen:frame()
+-- hs.hotkey.bind({"cmd", "alt"}, "f", function()
+--   local win = hs.window.focusedWindow()
+--   local screen = win:screen()
+--   local max = screen:frame()
 
-  win:setFrame(max)
-end)
+--   win:setFrame(max)
+-- end)
 
-local l = 2
-local r = 2
-local u = 2
-local d = 2
-local XY = {{x=1/3, y=2/3}, {x=1/2, y=1/2}, {x=2/3, y=1/3}}
+-- local l = 2
+-- local r = 2
+-- local u = 2
+-- local d = 2
+-- local XY = {{x=1/3, y=2/3}, {x=1/2, y=1/2}, {x=2/3, y=1/3}}
 
-resizeLeft = function()
-  r = 2
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+-- resizeLeft = function()
+--   r = 2
+--   local win = hs.window.focusedWindow()
+--   local f = win:frame()
+--   local screen = win:screen()
+--   local max = screen:frame()
 
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w * XY[l].x
-  f.h = max.h
-  win:setFrame(f)
-  if l < 3 then
-    l = l + 1
-   else
-    l = 1
-  end
-end
+--   f.x = max.x
+--   f.y = max.y
+--   f.w = max.w * XY[l].x
+--   f.h = max.h
+--   win:setFrame(f)
+--   if l < 3 then
+--     l = l + 1
+--    else
+--     l = 1
+--   end
+-- end
 
-resizeRight = function()
-  l = 2
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+-- resizeRight = function()
+--   l = 2
+--   local win = hs.window.focusedWindow()
+--   local f = win:frame()
+--   local screen = win:screen()
+--   local max = screen:frame()
 
-  f.x = max.x + (max.w * XY[r].y)
-  f.y = max.y
-  f.w = max.w * XY[r].x
-  f.h = max.h
-  win:setFrame(f)
-  if r < 3 then
-    r = r + 1
-   else
-    r = 1
-  end
-end
+--   f.x = max.x + (max.w * XY[r].y)
+--   f.y = max.y
+--   f.w = max.w * XY[r].x
+--   f.h = max.h
+--   win:setFrame(f)
+--   if r < 3 then
+--     r = r + 1
+--    else
+--     r = 1
+--   end
+-- end
 
-resizeUp = function()
-  d = 2
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+-- resizeUp = function()
+--   d = 2
+--   local win = hs.window.focusedWindow()
+--   local f = win:frame()
+--   local screen = win:screen()
+--   local max = screen:frame()
 
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w
-  f.h = max.h * XY[u].x
-  win:setFrame(f)
-  if u < 3 then
-    u = u + 1
-   else
-    u = 1
-  end
-end
+--   f.x = max.x
+--   f.y = max.y
+--   f.w = max.w
+--   f.h = max.h * XY[u].x
+--   win:setFrame(f)
+--   if u < 3 then
+--     u = u + 1
+--    else
+--     u = 1
+--   end
+-- end
 
-resizeDown = function()
-  u = 2
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+-- resizeDown = function()
+--   u = 2
+--   local win = hs.window.focusedWindow()
+--   local f = win:frame()
+--   local screen = win:screen()
+--   local max = screen:frame()
 
-  f.x = max.x
-  f.y = max.y + (max.h * XY[d].y)
-  f.w = max.w
-  f.h = max.h * XY[d].x
-  win:setFrame(f)
-  if d < 3 then
-    d = d + 1
-   else
-    d = 1
-  end
-end
+--   f.x = max.x
+--   f.y = max.y + (max.h * XY[d].y)
+--   f.w = max.w
+--   f.h = max.h * XY[d].x
+--   win:setFrame(f)
+--   if d < 3 then
+--     d = d + 1
+--    else
+--     d = 1
+--   end
+-- end
 
-hs.hotkey.bind({"cmd", "alt"}, "h", resizeLeft)
-hs.hotkey.bind({"cmd", "alt"}, "left", resizeLeft)
+-- hs.hotkey.bind({"cmd", "alt"}, "h", resizeLeft)
+-- hs.hotkey.bind({"cmd", "alt"}, "left", resizeLeft)
 
-hs.hotkey.bind({"cmd", "alt"}, "l", resizeRight)
-hs.hotkey.bind({"cmd", "alt"}, "right", resizeRight)
+-- hs.hotkey.bind({"cmd", "alt"}, "l", resizeRight)
+-- hs.hotkey.bind({"cmd", "alt"}, "right", resizeRight)
 
-hs.hotkey.bind({"cmd", "alt"}, "k", resizeUp)
-hs.hotkey.bind({"cmd", "alt"}, "up", resizeUp)
+-- hs.hotkey.bind({"cmd", "alt"}, "k", resizeUp)
+-- hs.hotkey.bind({"cmd", "alt"}, "up", resizeUp)
 
-hs.hotkey.bind({"cmd", "alt"}, "j", resizeDown)
-hs.hotkey.bind({"cmd", "alt"}, "down", resizeDown)
+-- hs.hotkey.bind({"cmd", "alt"}, "j", resizeDown)
+-- hs.hotkey.bind({"cmd", "alt"}, "down", resizeDown)
 
-hs.urlevent.httpCallback = function(scheme, host, params, fullURL)
-  hs.alert.show("clicked " .. fullURL)
-end
+-- hs.urlevent.httpCallback = function(scheme, host, params, fullURL)
+--   hs.alert.show("clicked " .. fullURL)
+-- end
