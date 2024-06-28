24677d88d7b08e4a976469e5692bb4a1b5d2b090
Stephen Laughton <stephenlaughton@gmail.com>
2024-06-27 17:24:01 -0700

    get rid of some stuff that isnt universal

--- install/brew.sh
+++ install/brew.sh
@@ -9,15 +9,15 @@ brew tap homebrew/cask-fonts
 brew install git
 brew install svn
 # Node stuff
-brew install nodenv
-# grab stable version of node
-nodenv install 14.19.3
-# convenience plugin for nodenv
-git clone https://github.com/nodenv/nodenv-package-rehash.git "$(nodenv root)"/pluginsnodenv-package-rehash
-git clone https://github.com/nodenv/nodenv-aliases.git $(nodenv root)/plugins/nodenv-aliases
-nodenv alias --auto
-nodenv package-hooks install --all
-brew install temurin11 # java runtime
+# brew install nodenv
+# # grab stable version of node
+# nodenv install 14.19.3
+# # convenience plugin for nodenv
+# git clone https://github.com/nodenv/nodenv-package-rehash.git "$(nodenv root)"/pluginsnodenv-package-rehash
+# git clone https://github.com/nodenv/nodenv-aliases.git $(nodenv root)/plugins/nodenv-aliases
+# nodenv alias --auto
+# nodenv package-hooks install --all
+# brew install temurin11 # java runtime
 brew install bash
 brew install carthage
 brew install eza
@@ -34,16 +34,16 @@ brew install blackhole-2ch
 # apps
 brew install 1password
 brew install alfred
-brew install altair-graphql-client
+# brew install altair-graphql-client
 brew install android-studio
 brew install appcleaner
 brew install daisydisk
-brew install discord
+# brew install discord
 brew install figma
 brew install firefox
 brew install google-chrome
-brew install hammerspoon
-brew install istat-menus
+# brew install hammerspoon
+# brew install istat-menus
 brew install iterm2
 brew install kap
 brew install linear-linear
@@ -51,7 +51,7 @@ brew install insomnia
 brew install notion
 brew install responsively
 brew install slack
-brew install telegram
+# brew install telegram
 brew install the-unarchiver
 brew install visual-studio-code-insiders
 brew install vlc
